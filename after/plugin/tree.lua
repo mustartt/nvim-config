7ac8a5aa83bcb19d7c5ae61b0cc050eab91b3b01
Henry Jiang <h243jian@uwaterloo.ca>
2024-05-30 23:12:02 -0400

    add keybind for nvim tree

--- after/plugin/tree.lua
+++ after/plugin/tree.lua
@@ -3,4 +3,28 @@ vim.g.loaded_netrwPlugin = 1
 
 vim.opt.termguicolors = true
 
-require("nvim-tree").setup({})
+local api = require "nvim-tree.api"
+
+local function my_on_attach(bufnr)
+  local function opts(desc)
+    return {
+        desc = "nvim-tree: " .. desc, 
+        buffer = bufnr, 
+        noremap = true, 
+        silent = true, 
+        nowait = true 
+    }
+  end
+
+  -- default mappings
+  api.config.mappings.default_on_attach(bufnr)
+
+  vim.keymap.set('n', '<C-t>', api.tree.change_root_to_parent,        opts('Up'))
+  vim.keymap.set('n', '?',     api.tree.toggle_help,                  opts('Help'))
+end
+
+vim.keymap.set('n', '<leader>tree', api.tree.toggle, {})
+
+require("nvim-tree").setup({
+  on_attach = my_on_attach
+})
