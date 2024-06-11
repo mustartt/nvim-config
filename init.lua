8c8cd6512b3763814e2066de96dc381d79321dc5
Henry Jiang <henry.jiang1@ibm.com>
2024-06-11 10:33:33 -0400

    Add llvm IR filetype

--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
 require("henry")
 
 vim.o.number = true
-vim.o.relativenumber = false
+vim.o.relativenumber = true
 vim.o.showmatch = true
 vim.o.ignorecase = true
 vim.o.cc = "80"
@@ -9,3 +9,9 @@ vim.o.tabstop = 4
 vim.o.shiftwidth = 4
 vim.o.softtabstop = 4
 vim.o.expandtab = true
+
+vim.filetype.add({
+    extension = {
+        ll = 'llvm'
+    }
+})
